d4d39dac75c05badc5e9bc05f13725d842000cc3
ladymorape <111259989+ladymorape@users.noreply.github.com>
2022-08-14 21:38:20 -0500

    Add files via upload

---
+++ reto5/src/main/java/reto5/Controlador/Controlador.java
@@ -0,0 +1,9 @@
+package reto5.Controlador;
+import reto5.Vista.*;
+
+public class Controlador {
+    public static void main( String[] args )
+    {
+        new InterfazP();    
+    }
+}
---
+++ reto5/src/main/java/reto5/Modelo/DAO/Consulta1DAO.java
@@ -0,0 +1,20 @@
+package reto5.Modelo.DAO;
+import java.sql.*;
+import reto5.Utilidades.*; 
+public class Consulta1DAO {
+    public static ResultSet consulta() {
+        ResultSet rs=null;
+        Statement stmt=null;
+        Connection conn=null;
+        String csql="SELECT ID_Lider,Nombre,Primer_Apellido, Ciudad_Residencia FROM Lider ORDER BY Ciudad_Residencia;";
+        try {
+            conn=JDBCUtilities.conexion();
+            stmt=conn.createStatement();
+            rs=stmt.executeQuery(csql);
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+        return rs;
+        
+    }   
+}
